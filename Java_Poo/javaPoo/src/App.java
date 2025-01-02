086ba063dd653305b2e32209c118761f279a81b0
Ebert Guerreiro <ebertg.est@gmail.com>
2025-01-02 20:43:54 -0300

    a

--- Java_Poo/javaPoo/src/App.java
+++ Java_Poo/javaPoo/src/App.java
@@ -4,14 +4,11 @@ public class App {
     public static void main(String[] args) throws Exception {
         SomarArea calcularArea = new SomarArea();
         Quadrado quadrado = new Quadrado(4);
-        Triangulo triangulo = new Triangulo(3, 3);
+         Triangulo triangulo = new Triangulo(3, 3);
         System.out.println(triangulo.calcularArear());
         System.out.println();
         System.out.println(quadrado.calcularArear());
         System.out.println(calcularArea.somarArea(triangulo, triangulo));
 
-        
-
-
-     }
+    }
 }
